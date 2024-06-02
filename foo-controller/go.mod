0e62814b4bef8fc352d94f202a74844774313915
costa92 <944310536@qq.com>
2024-06-02 23:08:45 +0800

    feat: add k8s-generator

---
+++ foo-controller/go.mod
@@ -0,0 +1,5 @@
+module github.com/costa92/k8s-generator-demo/foo-controller
+
+go 1.22.2
+
+require k8s.io/apimachinery v0.29.5 // indirect
---
+++ foo-controller/pkg/apis/foo/v1/doc.go
@@ -0,0 +1,5 @@
+
+// +k8s:deepcopy-gen=package
+// +groupName=foo.example.com
+
+package v1  // import "github.com/costa92/k8s-generator/foo-controller/pkg/apis/foo/v1"
